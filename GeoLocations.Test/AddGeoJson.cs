a2c3c7bc549e91200ef3f4dddf8b7460737a185d
Lionel Slade <Lionel_Slade@hotmail.com>
2023-04-03 15:29:54 +0000

    Added Test Solution

---
+++ GeoLocations.Test/AddGeoJson.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+using System.Reflection;
+
+namespace GeoLocations.Test
+{
+    [TestClass]
+    public class AddGeoJson
+    {
+        [TestMethod]
+        public void AddGeoLocations()
+        {
+            //string geoJsonFileName = AssemblyConfigurationAttribute. //JsonFilePathGeoLocations();
+
+            //InsertJsonToSql(geoJsonFileName,  )
+            Debug.WriteLine("Here");
+        }
+    }
+}
---
+++ GeoLocations.Test/Configurations.cs
@@ -0,0 +1,24 @@
+﻿//using GeoLocations.Services;
+using GeoLocations.Services;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace GeoLocations.Test
+{
+    [TestClass]
+    public class Configurations
+    {
+        [TestMethod]
+        public void CheckConfiguration()
+        {
+            Debug.WriteLine("Here");
+
+            var configurations = new Configuration().Configurations;
+            //Assert.IsNotNull(configurations);
+            //Assert.IsTrue(configurations.Count() > 0);
+            //var JsonFilePathGeoLocations = configurations[0];
+            //Assert.IsNotNull(JsonFilePathGeoLocations.Contains("geoLocations.geojson"));
+
+        }
+    }
+}
